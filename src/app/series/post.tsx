e70323a10f6ccccf4613d5c1551defefca898cec
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-04-17 17:47:46 +0700

    up

--- src/app/series/post.tsx
+++ src/app/series/post.tsx
@@ -1,3 +1,35 @@
+import { title } from "process";
+
 export default function Post() {
-  return <>Post</>;
+  return (
+    <>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <div>{post.publicDate}</div>
+        </div>
+      ))}
+    </>
+  );
 }
+const posts = [
+  {
+    id: 1,
+    title: "Post 1",
+    content: "Content 1",
+    publicDate: new Date().toString(),
+  },
+  {
+    id: 1,
+    title: "Post 1",
+    content: "Content 1",
+    publicDate: new Date().toString(),
+  },
+  {
+    id: 1,
+    title: "Post 1",
+    content: "Content 1",
+    publicDate: new Date().toString(),
+  },
+];

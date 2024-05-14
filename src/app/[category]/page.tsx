e823e6d592621b3f786c0bba891a3340b4a4a2d2
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-05-14 17:23:13 +0700

    up

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,12 +1,23 @@
-"use client";
+// "use client";
 
 import ListPost from "./ListPost";
 import { layout_styles } from "./style";
 
-export default function PostPage({ params }: { params: { category: string } }) {
+export default async function PostPage({
+  params,
+}: {
+  params: { category: string };
+}) {
+  // console.log("category ", params.category);
+  const url = "http://localhost:1337/api/post-category?tag=layer1&category=1";
+  const res = await fetch(url);
+  const posts = await res.json();
+  console.log("post: ", posts);
+
   return (
     <div className="w">
       {/* <h2 style={layout_styles}>{params.slug}</h2> */}
+
       <ListPost category={params.category}></ListPost>
     </div>
   );

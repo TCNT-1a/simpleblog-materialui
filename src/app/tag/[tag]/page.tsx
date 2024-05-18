1ac50a8399acaae29a4b4e3eb2069c81be4a1b04
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-05-19 00:25:06 +0700

    up

--- src/app/tag/[tag]/page.tsx
+++ src/app/tag/[tag]/page.tsx
@@ -8,9 +8,8 @@ type Props = {
 };
 
 export default async function TagPage({ params, searchParams }: Props) {
-  console.log("aa", params.tag);
   const { data } = await getApi2(`api/blog/post?tag=${params.tag}`);
-  console.log(data);
+
   const { posts } = data;
 
   return (

bf80b8c07ae6229e0baa322b1bfb2ae25fcd6392
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-05-22 17:40:25 +0700

    up

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,13 @@
-"use client";
 import { TextInput } from "flowbite-react";
 import Brand from "./brand";
 import { BRANCH_NAME } from "@/config/app.config";
-export default function Header() {
+import { getPageInfo } from "@/config/api-helper";
+export default async function Header() {
+  const pageInfo = await getPageInfo();
+
   return (
     <div className="bg-primary flex justify-between items-center">
-      <Brand branchName={BRANCH_NAME}></Brand>
+      <Brand branchName={pageInfo.branchName || BRANCH_NAME}></Brand>
       <div className="p-3">
         <TextInput type="text" placeholder="search" />
       </div>
